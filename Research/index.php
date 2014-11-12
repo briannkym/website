51aa5124b6d0546943246d39ac5517fa327c7a08
Brian Nakayama <nakayama@iastate.edu>
2014-11-12 10:22:28 -0600

    first one, alright alright!

---
+++ Research/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Brian Nakayama: Computer Science Enthusiast</title>
+    </head>
+    <?php include($_SERVER['DOCUMENT_ROOT'].'/Style/header.php'); ?>
+    <br>   
+    <h1>Previous Research</h1>
+    <br><br><br>
+    <h2>Universal Computation in the Prisoner's Dilemma Game</h2><br>
+    This thesis examines emergent complexity and universal computation in spatio
+    -temporal interactions of the iterated Prisoner’s Dilemma Game (IPDG). Using
+    a Cellular Automata simulation, cells hold different deterministic strategies 
+    for playing the IPDG. Using these strategies, cells accumulate points. They then copy
+    the strategies of neighboring cells whenever a neighboring cell makes more points. A 
+    proof of holistic universal computation (Turing Machine equivalence) is shown, 
+    followed by a discussion of the proof's greater implications. 
+    <br><br>
+    Unfortunately the paper will no longer be available until it is published*, but 
+    you can download the program here: 
+    <br><br><div align="center">
+    <a href ="Download.zip">PDG CA Simulator</a>
+    <br><br>
+    <h3>D-Latch Made Out of the PDG CA</h3>
+    <iframe width="640" height="360" src="http://www.youtube.com/embed/XvrFHsIMhfA" frameborder="0" allowfullscreen></iframe>
+    </div><br><br>
+    *for anyone who downloaded the third draft, it had a few errors in it.
+    <br><br>
+    Also, this is my first time using the GNU license. If I missed anything or anyone
+    has any pointers, I would appreciate your <a href="../ContactMe">advice</a>.
+    <br><br><br><br><br><br>
+    
+    <?php include($_SERVER['DOCUMENT_ROOT'].'/Style/footer.php'); ?>
+</html>
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+   header( 'Location: http://www.briannakayama.com/Home' );
+?>
